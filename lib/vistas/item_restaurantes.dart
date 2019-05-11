6a64e36b0b74c8a69a5c2c9101b253222826b2a9
FenriRuiz <32452140+FenriRuiz@users.noreply.github.com>
2019-05-11 14:32:09 +0200

    Arreglado lista de restaurantes

--- lib/vistas/item_restaurantes.dart
+++ lib/vistas/item_restaurantes.dart
@@ -3,73 +3,73 @@ import 'package:the_good_plate/modelos/modelo_restaurantes.dart';
 import 'package:the_good_plate/rutas/restaurante.dart';
 
 class ItemRestaurante extends StatefulWidget {
-  ItemRestaurante(ModeloRestaurante _restaurante);
+  final ModeloRestaurante _restaurante;
+  ItemRestaurante(this._restaurante);
 
-  ItemRestauranteState createState() => new ItemRestauranteState();
+  @override
+  _ItemRestauranteState createState() => _ItemRestauranteState(_restaurante);
 }
-class ItemRestauranteState extends State<ItemRestaurante>{
-  ModeloRestaurante _restaurante;
-  //ItemRestaurante(this._restaurante);
+
+class _ItemRestauranteState extends State<ItemRestaurante> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-//TODO: Falta el body
- @override
-    Widget build(BuildContext context) {
-      return new InkWell(
-          onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Restaurante(
-                        _restaurante,
-                      ))),
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(25),
-              child: Card(
-                color: Color.fromRGBO(60, 190, 200, 100),
-                child: new Column(
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-                        child: Hero(
-                          child: FadeInImage.assetNetwork(
-                            placeholder: 'images/loading.gif',
-                            image: _restaurante.imagen,
-                          ),
-                          tag: _restaurante.imagen,
+
+  _ItemRestauranteState(ModeloRestaurante _restaurante);
+
+  Widget build(BuildContext context) {
+    return new InkWell(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context)=> Restaurante(widget._restaurante,))),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child: Card(
+              color: Color.fromRGBO(60, 190, 200, 100),
+              child: new Column(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      child: Hero(
+                        child: FadeInImage.assetNetwork(
+                          
+                          placeholder: 'images/loading.gif',
+                          image: widget._restaurante.imagen,
                         ),
+                        tag: widget._restaurante.imagen,
                       ),
                     ),
-                    new Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-                      child: Text(
-                        _restaurante.nombre,
-                        style: style,
-                        softWrap: true,
-                      ),
+                  ),
+                  new Container(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+                    child: Text(
+                      widget._restaurante.nombre,
+                      style: style,
+                      softWrap: true,
                     ),
-                    new Container(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
-                      child: Text(
-                        _restaurante.descripcion,
-                        style:
-                            TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
-                        softWrap: true,
-                      ),
+                  ),
+                  new Container(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 5), 
+                    child: Text(
+                      widget._restaurante.descripcion,
+                      style:
+                          TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
+                      softWrap: true,
                     ),
-                    new Padding(
-                        padding: new EdgeInsets.all(7.0),
-                        child: new Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            new Icon(Icons.star_border,
-                                size: 40.0, color: Colors.amberAccent),
-                            new Icon(Icons.favorite_border,
-                                size: 40.0, color: Colors.redAccent),
-                          ],
-                        ))
-                  ],
-                ),
-              )));
-    }
+                  ),
+                  new Padding(
+                      padding: new EdgeInsets.all(7.0),
+
+                      child: new Row(
+
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          
+                          new Icon(Icons.star_border, size: 40.0, color: Colors.amberAccent),
+                          new Icon(Icons.favorite_border,size: 40.0, color: Colors.redAccent),
+                          
+                        ],
+                      ))
+                ],
+              ),
+            )));
   }
+}
